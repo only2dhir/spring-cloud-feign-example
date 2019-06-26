501ce929fbf9c55ee00c92216b1ec0e081499cdb
only2 <only2dhir@gmail.com>
2019-06-27 00:01:58 +0530

    Added custom config

--- customer-service/src/main/java/com/devglan/customerservice/feign/config/CustomFeignConfig.java
+++ customer-service/src/main/java/com/devglan/customerservice/feign/config/CustomFeignConfig.java
@@ -1,6 +1,25 @@
 package com.devglan.customerservice.feign.config;
 
+import feign.Contract;
+import feign.auth.BasicAuthRequestInterceptor;
+import feign.okhttp.OkHttpClient;
+import org.springframework.context.annotation.Bean;
+
 public class CustomFeignConfig {
 
+    @Bean
+    public Contract feignContract() {
+        return new feign.Contract.Default();
+    }
+
+    @Bean
+    public BasicAuthRequestInterceptor basicAuthRequestInterceptor() {
+        return new BasicAuthRequestInterceptor("user", "password");
+    }
+
+    @Bean
+    public OkHttpClient client() {
+        return new OkHttpClient();
+    }
 
 }
